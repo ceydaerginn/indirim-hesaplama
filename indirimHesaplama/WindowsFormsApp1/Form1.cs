cb8b5de30b7a548fcb128ae7cc352e126eeb9262
ceyda <154838176+ceydaerginn@users.noreply.github.com>
2024-08-18 01:28:34 +0300

    Add files via upload

---
+++ indirimHesaplama/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double fiyat;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fiyat = Convert.ToDouble(textBox1.Text);
+            label3.Text = Convert.ToString(fiyat - (fiyat * 10 / 100));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fiyat = Convert.ToDouble(textBox1.Text);
+            label3.Text = Convert.ToString(fiyat - (fiyat * 25 / 100));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fiyat = Convert.ToDouble(textBox1.Text);
+            label3.Text = Convert.ToString(fiyat - (fiyat * 50 / 100));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fiyat = Convert.ToDouble(textBox1.Text);
+            label3.Text = Convert.ToString(fiyat - (fiyat * 75 / 100));
+        }
+    }
+}
